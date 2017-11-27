4882a6b3538542f175fb014a3d18698a3c433827
Vinicius Rufino <vrufino@sweda.com.br>
2017-11-27 14:52:12 -0200

    droidVNCServer - Android 5.x + ZTE

---
+++ app/build/generated/source/buildConfig/debug/org/onaips/vnc/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.onaips.vnc;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.onaips.vnc";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 58;
+  public static final String VERSION_NAME = "1.1RC1";
+  // Fields from default config.
+  public static final int VALUE = 1;
+}
